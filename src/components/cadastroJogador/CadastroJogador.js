37533321488d04c6531092a64e0836806d4b99f2
annabel30 <annabel_wulf@estudante.sesisenai.org.br>
2023-05-15 21:48:30 -0300

    not allowed equal names

--- src/components/cadastroJogador/CadastroJogador.js
+++ src/components/cadastroJogador/CadastroJogador.js
@@ -19,32 +19,36 @@ function CadastroJogador() {
   })
 
   const atualizaPlayer = (event) => {
-    setPlayer({...player, [event.target.name] : event.target.value})
+    setPlayer({ ...player, [event.target.name]: event.target.value })
   }
 
-  function mostrarAlertCadastroJogador() {
+  const cadastrar = (event) => {
 
+    event.preventDefault()
     let verifyPassword = document.getElementById("confirmPasswordJogadorInput")
-    
+
     if (player.name !== '' && player.age !== ''
       && player.gender !== '' && player.region !== ''
       && player.element !== '' && player.weapon !== ''
       && player.password !== '') {
-      if (player.password === verifyPassword.value) {
-        alert("Cadastro efetuado!");
-        window.location.href = '/';
-      } else {
-        alert("Senhas não compatíveis");
-      }
+      PlayerService.buscarPorNome(player.name).then((resposta) => {
+        if (player.name == resposta.data.name) { // eslint-disable-line eqeqeq
+          alert("Nome já existente!");
+          window.location.reload();
+        }
+      }).catch((erro) => {
+        console.log(erro)
+        if (player.password === verifyPassword.value) {
+          PlayerService.cadastrar(player)
+          alert("Cadastro efetuado!");
+          window.location.href = '/';
+        } else {
+          alert("Senhas não compatíveis");
+        }
+      })
     } else {
       alert("Insira todos os valores");
     }
-  }
-
-  const cadastrar = (event) => {
-    event.preventDefault()
-    mostrarAlertCadastroJogador()
-    PlayerService.cadastrar(player)
   }
 
   return (
@@ -57,19 +61,19 @@ function CadastroJogador() {
         <Form className='boxJogadorCadastro' onSubmit={cadastrar}>
           <Form.Group className="mb-3" id="formNamePlayer">
             <Form.Label>Nome do viajante</Form.Label>
-            <Form.Control type="text" placeholder="Insira o nome" id='nomeJogadorInput' 
-            name ='name' onChange={atualizaPlayer} value={player.name} />
+            <Form.Control type="text" placeholder="Insira o nome" id='nomeJogadorInput'
+              name='name' onChange={atualizaPlayer} value={player.name} />
           </Form.Group>
 
           <Form.Group className="mb-3" id="formAgePlayer">
             <Form.Label>Idade</Form.Label>
             <Form.Control type="number" placeholder="Idade" id='idadeJogadorInput'
-            name ='age' onChange={atualizaPlayer} value={player.age} />
+              name='age' onChange={atualizaPlayer} value={player.age} />
           </Form.Group>
 
           <Form.Group className="mb-3" id="formGenderPlayer">
             <Form.Select className='selectGenderPlayer' id='generoJogadorInput'
-            name ='gender' onChange={atualizaPlayer} value={player.gender}>
+              name='gender' onChange={atualizaPlayer} value={player.gender}>
               <option defaultValue={''} disabled hidden value="">Gênero</option>
               <option value="F">Feminino</option>
               <option value="M">Masculino</option>
@@ -78,7 +82,7 @@ function CadastroJogador() {
 
           <Form.Group className="mb-3">
             <Form.Select className='selectRegionPlayer' id='regionJogadorInput'
-            name ='region' onChange={atualizaPlayer} value={player.region}>
+              name='region' onChange={atualizaPlayer} value={player.region}>
               <option defaultValue={''} disabled hidden value="">Região</option>
               <option value="Mondstadt">Mondstadt</option>
               <option value="Liyue">Liyue</option>
@@ -89,7 +93,7 @@ function CadastroJogador() {
 
           <Form.Group className="mb-3">
             <Form.Select className='selectElementPlayer' id='elementJogadorInput'
-            name ='element' onChange={atualizaPlayer} value={player.element}>
+              name='element' onChange={atualizaPlayer} value={player.element}>
               <option defaultValue={''} disabled hidden value="">Elemento</option>
               <option value="Anemo">Anemo</option>
               <option value="Cryo">Cryo</option>
@@ -103,7 +107,7 @@ function CadastroJogador() {
 
           <Form.Group className="mb-3">
             <Form.Select className='selectWeaponPlayer' id='weaponJogadorInput'
-            name ='weapon' onChange={atualizaPlayer} value={player.weapon}>
+              name='weapon' onChange={atualizaPlayer} value={player.weapon}>
               <option defaultValue={''} disabled hidden value="">Arma</option>
               <option value="Bow">Bow</option>
               <option value="Catalyst">Catalyst</option>
@@ -116,12 +120,12 @@ function CadastroJogador() {
           <Form.Group className="mb-3" id="formPasswordPlayer">
             <Form.Label>Criar senha</Form.Label>
             <Form.Control type="password" placeholder="Senha" id='passwordJogadorInput'
-            name ='password' onChange={atualizaPlayer} value={player.password} />
+              name='password' onChange={atualizaPlayer} value={player.password} />
           </Form.Group>
 
           <Form.Group className="mb-3" id="formConfirmPasswordPlayer">
             <Form.Label>Confirmar senha</Form.Label>
-            <Form.Control type="password" placeholder="Senha" id='confirmPasswordJogadorInput'/>
+            <Form.Control type="password" placeholder="Senha" id='confirmPasswordJogadorInput' />
           </Form.Group>
 
           <Button className='botaoAdicionarJogador' variant="primary" type="submit" onClick={cadastrar}>
