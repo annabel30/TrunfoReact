77211fdab951198e93ce140c9946e308fd43f0e7
annabel30 <annabel_wulf@estudante.sesisenai.org.br>
2023-05-18 16:37:38 -0300

    improved game

--- src/components/jogo/Jogo.js
+++ src/components/jogo/Jogo.js
@@ -2,17 +2,170 @@ import "./Jogo.css"
 
 import CardBack from "../../img/Extras/cardBack.png"
 
+import Carta from '../carta/Carta';
+import CartaJogo from '../cartaJogo/CartaJogo';
+
+import { v4 } from 'uuid';
+import { useEffect, useState } from "react";
+import { CardService } from '../../service/CardService';
+
 function Jogo() {
+
+    // ----------------------------- METODOS DECK -----------------------------
+    const [card, setCard] = useState([])
+    const [listAllCards, setListAllCards] = useState([])
+    const [listCardsOpponent, setListCardsOpponent] = useState([])
+    const [listCardsPlayer, setListCardsPlayer] = useState([])
+    const [playerCardModel, setPlayerCardModel] = useState(null)
+    const [opponentCardModel, setOpponentCardModel] = useState(null)
+
+    // ----------------------------- METODOS JOGO -----------------------------
+    const [startedMatch, setStartedMatch] = useState(0);
+
+    useEffect(() => {
+        separateCards()
+        game()
+    }, [])
+
+    useEffect(() => {
+        console.log("lista player")
+        console.log(listCardsPlayer);
+        console.log("lista oponente")
+        console.log(listCardsOpponent);
+        setCard(listCardsPlayer[0])
+    }, [listCardsPlayer]);
+
+    // ----------------------------- METODOS DECK -----------------------------
+    const separateCards = () => {
+        CardService.buscarTodasCards().then(resposta => {
+            setListCardsOpponent([]);
+            setListCardsPlayer([]);
+            resposta.data.sort(() => Math.random() - 0.5);
+            const deckCards = resposta.data;
+            setListAllCards(deckCards)
+            for (let i = 0; i < deckCards.length; i++) {
+                if (i % 2 === 0) {
+                    setListCardsPlayer((prevDeck) => [...prevDeck, deckCards[i]]);
+                } else {
+                    setListCardsOpponent((prevDeck) => [...prevDeck, deckCards[i]]);
+                }
+            }
+        }).catch(erro =>
+            console.log(erro)
+        )
+    }
+
+    const createModelCards = () => {
+        setPlayerCardModel(listCardsPlayer[0]);
+        setOpponentCardModel(listCardsOpponent[0]);
+    };
+
+    const computerWonRound = () => {
+        createModelCards();
+        addComputerDeck(playerCardModel);
+        removeUserDeck();
+        removeComputerDeck();
+        addComputerDeck(opponentCardModel);
+    };
+
+    const playerWonRound = () => {
+        createModelCards();
+        addUserDeck(opponentCardModel);
+        removeComputerDeck();
+        removeUserDeck();
+        addUserDeck(playerCardModel);
+    };
+
+    const addUserDeck = (card) => {
+        setListCardsPlayer((prevDeck) => [...prevDeck, card]);
+    };
+
+    const addComputerDeck = (card) => {
+        setListCardsOpponent((prevDeck) => [...prevDeck, card]);
+    };
+
+    const removeUserDeck = () => {
+        setListCardsPlayer((prevDeck) => prevDeck.slice(1));
+    };
+
+    const removeComputerDeck = () => {
+        setListCardsOpponent((prevDeck) => prevDeck.slice(1));
+    };
+
+    const getUserDeck = () => {
+        return listCardsPlayer;
+    };
+
+    const getComputerDeck = () => {
+        return listCardsOpponent;
+    };
+
+    // ----------------------------- METODOS GAME -----------------------------
+    const game = () => {
+        setStartedMatch(Math.floor(Math.random() * 2));
+
+        do {
+            if (startedMatch === 0) {
+                playerPlays();
+                setStartedMatch(1);
+            } else {
+                computerPlays();
+                setStartedMatch(0);
+            }
+        } while (listCardsOpponent.length !== 0 || listCardsPlayer.length !== 0);
+
+        // if (listCardsPlayer.length === 0) {
+        //     setPlayerModel((prevModel) => ({
+        //         ...prevModel,
+        //         defeats: prevModel.defeats + 1,
+        //     }));
+        // } else if (listCardsOpponent.length === 0) {
+        //     setPlayerModel((prevModel) => ({
+        //         ...prevModel,
+        //         victories: prevModel.victories + 1,
+        //     }));
+        // }
+    }
+
+    const computerPlays = () => {
+        const randomAttribute = Math.floor(Math.random() * 5);
+        match(randomAttribute + 1);
+    };
+
+    const playerPlays = () => {
+        const chosenAttribute = 0; // Set your chosen attribute logic here
+        match(chosenAttribute);
+    };
+
+    const match = (chosenAtt) => {
+        const wonRound = compareAttributes(chosenAtt);
+
+        if (wonRound) {
+            playerWonRound();
+        } else {
+            computerWonRound();
+        }
+    };
+
+    const compareAttributes = (chosenAtt) => {
+        let attributeUserValue = 0;
+        let attributeComputerValue = 0;
+
+        
+    };
+
     return (
         <>
-            <div>
-                <a className='aLogin' href='./regrasJogo'>
-                    <div className='optionVoltarPerfilJogador'>Desistir</div>
-                </a>
-                <div>Cartas restantes: 0</div>
-                <div>Cartas restantes: 0</div>
-                <img src={CardBack} className="cardBack" alt="" />
-                <img src={CardBack} className="cardBack" alt="" />
+            <div className='jogoWallpaper'></div>
+            <a className='aLogin' href='./regrasJogo'>
+                <div className='optionDesistirJogo'>Desistir</div>
+            </a>
+            <div className="boxGame">
+                <div className="cartasRestantesPlayer">Cartas restantes: {listCardsPlayer.length}</div>
+                <div className="cartasRestantesOponent">Cartas restantes: {listCardsOpponent.length}</div>
+                <img src={CardBack} className="cardBackOponent" alt="" />
+                <img src={CardBack} className="cardBackPlayer" alt="" />
+                {card && <CartaJogo carta={card} borderColor={card.borderColor} attributeColor={card.attributeColor} key={v4()} />}
             </div>
         </>
     )
--- src/components/regrasJogo/RegrasJogo.js
+++ src/components/regrasJogo/RegrasJogo.js
@@ -1,8 +1,11 @@
 import "./RegrasJogo.css"
 
+import Navbar from "../navbar/Navbar";
+
 function RegrasJogo() {
     return (
         <>
+            <Navbar />
             <div className="pageShowRulesGame">
                 <div className="boxRulesGame">
                     <h1 className='titleCadastrarJogador'>Regras do Jogo</h1>
@@ -12,7 +15,7 @@ function RegrasJogo() {
                         when an unknown printer took a galley of type and scrambled
                         it to make a type specimen book.
                     </p>
-                    <a  className="aTravel" href="./jogo">
+                    <a className="aTravel" href="./jogo">
                         <button className='botaoAdicionarJogador' variant="primary" type="submit">
                             Iniciar jogo
                         </button>
